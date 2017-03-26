495e1f1e9ea89ab9afcfedc1a47a14e8062f37bc
bgreen <bw_green@yahoo.com>
2017-03-26 17:37:09 -0400

    add editing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,12 +3,26 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
   def create
     @post = Post.new(params.require(:post).permit(:content))
     if @post.save
       redirect_to @post
     else
       render 'new'
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
     end
   end
 
